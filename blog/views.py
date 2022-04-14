6ba3ee473862eca7e3f48f02f4af8a13a06ab7ad
KulyashDahiya <kulyashdahiya77@gmail.com>
2022-04-14 16:45:24 +0530

    accessed blog page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def all_blogs(request):
-    return render(request, '/blog/all_blogs.html')
+def index(request):
+    return render(request, 'blog/all_blogs.html')
